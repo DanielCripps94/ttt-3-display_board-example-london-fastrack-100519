6a58f260e7b09aa1e286f3afb81fff7af1c05e86
Daniel Cripps <dan-cripps@hotmail.co.uk>
2019-10-06 08:04:28 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,3 @@
-      puts "   |   |   "
-      puts "-----------"
-      puts "   |   |   "
-      puts "-----------"
-      puts "   |   |   "
+def display_board
+  puts "A Tic Tac Toe Board"
+  end
